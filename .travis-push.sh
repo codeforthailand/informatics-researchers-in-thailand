4fe894b1f2541c3ea19cdd581eeef6a11728c68d
heytitle <pat.chormai@gmail.com>
2019-08-01 00:16:03 +0700

    test build

--- .travis-push.sh
+++ .travis-push.sh
@@ -11,7 +11,7 @@ commit_country_json_files() {
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
   # Stage the modified files in dist/output
-  git add -f dist/output/*.json
+  git add -f README.md
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference

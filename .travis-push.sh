ddd855d12d00a5f671498fcef7a4daeac9fdb72b
heytitle <pat.chormai@gmail.com>
2019-08-01 00:30:47 +0700

    update

--- .travis-push.sh
+++ .travis-push.sh
@@ -6,7 +6,7 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_country_json_files() {
+commit_files() {
   git checkout master
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
@@ -18,22 +18,16 @@ commit_country_json_files() {
   git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 }
 
-upload_files() {
-  # Remove existing "origin"
-  git remote rm origin
-  # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://heytitle:${GH_TOKEN}@github.com/codeforthailand/informatics-researchers-in-thailand.git > /dev/null 2>&1
-  git push origin master --quiet
-}
-
 setup_git
 
-commit_country_json_files
+commit_files
 
 # Attempt to commit to git only if "git commit" succeeded
 if [ $? -eq 0 ]; then
-  echo "A new commit with changed country JSON files exists. Uploading to GitHub"
-  upload_files
+  echo "A new commit with changed exists. Uploading to GitHub"
+  git remote rm origin \
+    && git remote add origin https://heytitle:${GH_TOKEN}@github.com/codeforthailand/informatics-researchers-in-thailand.git > /dev/null 2>&1 \
+    && git push origin master --quiet
 else
   echo "No changes in country JSON files. Nothing to do"
 fi
